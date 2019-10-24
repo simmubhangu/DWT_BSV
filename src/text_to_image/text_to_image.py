25d166b9f7ac5d195bd9421b83548ff732d7860f
simmubhangu <simranjeet@cse.iitb.ac.in>
2019-10-25 00:58:01 +0530

    text to image added

---
+++ src/text_to_image/text_to_image.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+version_check =[0,0]   
+image_path = 'hh.txt'
+
+image_h = 128 
+image_w =128
+img = np.zeros((128,128),dtype=np.uint8)
+# print img
+def check():
+	i =0
+	j =0
+	p =0
+	while True:
+		with open(image_path) as f:  
+			textfile = f.readlines()             
+		for pixel_value in textfile:                     
+			if (i != image_h):
+				if(j != image_w-1):
+					img[i][j] = pixel_value
+					j = j+1
+				else:
+					img[i][j] = pixel_value
+					j=0
+					i= i+1
+		print type(img[0][0])
+		cv2.imshow("ll",img)
+		if cv2.waitKey(1) & 0xff == ord('q'):
+			break
+
+  
+if __name__ == '__main__':
+    check()
+    cv2.destroyAllWindows()
+ 
